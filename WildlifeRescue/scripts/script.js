2f3de930d83a8dd61c85dcdd066754efd85c33cd
bradmj17 <102694046+bradmj17@users.noreply.github.com>
2022-04-27 12:52:11 -0600

    attempt1
    brad
    cwi
    4/27/22

---
+++ WildlifeRescue/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Bradley Johnson
+    File Name: script.js
+    Date: 4/27/2022
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
