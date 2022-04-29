83cde753b65881f36847119a69d1b24a13df5028
bradmj17 <102694046+bradmj17@users.noreply.github.com>
2022-04-29 09:31:11 -0600

    example1

---
+++ BSportfolioStart/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: Bradley Johnson
+    File Name: script.js
+    Date: 4/25/22
+*/
+
+
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
+$(document).ready(function(){
+    $('.second').height($(window*.5).height());
+});
+
+/*-----*/
