283c30184e1501d56f67e3fc73b9ca176572d981
bradmj17 <102694046+bradmj17@users.noreply.github.com>
2022-08-26 16:36:36 -0600

    GLOAM

---
+++ Project1/scripts/newsletter.js
@@ -0,0 +1,29 @@
+"use strict";
+            
+// prompts visitor to submit email
+    let message;
+    let email = (prompt("Sign Up For the GLOAMingo Newsletter for 15% off!"+"\n\nSubmit your email here:"));
+
+if(email==null||email=="") //if they click cancel or just put nothing
+{
+ message = "Sign up for the Newsletter!";
+}
+else{
+     message = "Thank you! " +" "+ "A confirmation email has been sent to:" + "<br>" + email;
+}
+
+// display results
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt
+//it finds a certain ID (#) and puts the string in there, not just randomly on the page
+document.getElementById("email").innerHTML = message;
+
+
+
+
+
+
+
+//------LATER???
+
+//check if it has @ and . [@gmail.com] it cant be .com because there's other domains so it's hard to validate
+//let validate = email.inlcudes("@","!")
